2709b248a0b7cd5a0f5f84c2e2068c4ba9b09feb
JeffyLI <780446259@qq.com>
2019-09-09 09:16:26 +0800

    添加电子书网站收藏地址

--- tools/controller/collectionView.py
+++ tools/controller/collectionView.py
@@ -8,10 +8,21 @@ def collection_html(request):
                 {"name":"GitHub", "href":"https://github.com/"},
                 {"name":"知乎", "href":"https://www.zhihu.com"},
                 {"name": "哔哩哔哩", "href": "https://www.bilibili.com/"},
+                {"name": "腾讯云", "href": "https://cloud.tencent.com/"},
             ]},
-            {"title": "学习", "id": "collapse2", "collapses": [
+            {"title": "工具", "id": "collapse2", "collapses": [
                 {"name":"网站导航(一个开始)", "href":"https://aur.one/"},
-                {"name":"力扣", "href":"https://leetcode-cn.com/"}
+                {"name":"在线PS", "href":"https://ps.gaoding.com/#/?hmsr=zc-cc"},
+                {"name":"JS在线测试","href":"https://www.w3school.com.cn/tiy/t.asp?f=html_basic"}, 
+            ]},
+            {"title": "电子书", "id": "collapse3", "collapses": [
+		{"name":"epubw(优质)", "href":"https://epubw.com/"},
+                {"name":"万千合集站(学术)", "href":"http://www.hejizhan.com/bbs/"},
+                {"name":"周读(有评分)", "href":"http://www.ireadweek.com/"},
+		{"name":"田间小站(英文)", "href":"https://www.tianfateng.cn/"},
+		{"name":"杂志", "href":"http://www.ifblue.net/"},
+		{"name":"E书联盟", "href":"https://book118.com/"},
+		{"name":"搜索引擎汇总", "href":"https://ebook.chongbuluo.com/"},
             ]},
         ],
         "msg": "success",
