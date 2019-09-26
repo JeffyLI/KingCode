afa4ee60fc71406a8e0923b80813633abd9ddc5c
Jeffy <780446259@qq.com>
2019-09-27 00:15:40 +0800

    界面优化

--- tools/controller/notebookView.py
+++ tools/controller/notebookView.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def notebook_html(request):
+def list_html(request):
     context = {}
-    return render(request,'notebook.html', context)
+    return render(request, 'notebook/list.html', context)
+
+
+def edit_html(request):
+    context={}
+    return render(request, 'notebook/edit.html', context)
+
+
+def notebook_save(request):
+    print(request.POST['text'])
+    print(request.POST['html'])
+    return HttpResponse("success")
+
--- tools/urls.py
+++ tools/urls.py
@@ -6,6 +6,8 @@ from .controller import collectionView,faceRecognitionView,notebookView
 urlpatterns = [
     path('index',views.index),
     path('collection',collectionView.collection_html),
-    path('notebook',notebookView.notebook_html),
+    path('notebook',notebookView.list_html),
+    path('notebook/edit',notebookView.edit_html),
+    path('notebook/save',notebookView.notebook_save),
     path('face_recognition',faceRecognitionView.face_recognition_html),
 ]
